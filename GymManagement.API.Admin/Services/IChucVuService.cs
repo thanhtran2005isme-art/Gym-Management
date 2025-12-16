dcb28281f5931e1077fcd3a3a33f2b92bdfabe0c
thanhtran2005isme-art <thanhtran2005isme-art@users.noreply.github.com>
2025-12-16 08:45:00 +0700

    thêm IChucVuService

---
+++ GymManagement.API.Admin/Services/IChucVuService.cs
@@ -0,0 +1,13 @@
+using GymManagement.API.Admin.DTOs;
+
+namespace GymManagement.API.Admin.Services
+{
+    public interface IChucVuService
+    {
+        Task<IEnumerable<ChucVuDto>> GetAllAsync();
+        Task<ChucVuDto?> GetByIdAsync(int id);
+        Task<ChucVuDto> CreateAsync(ChucVuDto dto);
+        Task<ChucVuDto?> UpdateAsync(int id, ChucVuDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
