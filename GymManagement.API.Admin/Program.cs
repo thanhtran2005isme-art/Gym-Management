32cc21bc9bb4544e4c196ca730d30d2f9f2b66bb
thanhtran2005isme-art <thanhtran2005isme-art@users.noreply.github.com>
2025-12-18 11:36:19 +0700

    Update Program.cs

--- GymManagement.API.Admin/Program.cs
+++ GymManagement.API.Admin/Program.cs
@@ -4,6 +4,17 @@ using GymManagement.API.Admin.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// CORS cho Angular frontend
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -22,6 +33,9 @@ builder.Services.AddScoped<IGoiTapRepository, GoiTapRepository>();
 builder.Services.AddScoped<INhanVienRepository, NhanVienRepository>();
 builder.Services.AddScoped<IDangKyGoiTapRepository, DangKyGoiTapRepository>();
 builder.Services.AddScoped<IGoiPTRepository, GoiPTRepository>();
+builder.Services.AddScoped<IHoaDonRepository, HoaDonRepository>();
+builder.Services.AddScoped<IThanhToanRepository, ThanhToanRepository>();
+builder.Services.AddScoped<IDiemDanhRepository, DiemDanhRepository>();
 
 // Services
 builder.Services.AddScoped<IChucVuService, ChucVuService>();
@@ -30,6 +44,9 @@ builder.Services.AddScoped<IGoiTapService, GoiTapService>();
 builder.Services.AddScoped<INhanVienService, NhanVienService>();
 builder.Services.AddScoped<IDangKyGoiTapService, DangKyGoiTapService>();
 builder.Services.AddScoped<IGoiPTService, GoiPTService>();
+builder.Services.AddScoped<IHoaDonService, HoaDonService>();
+builder.Services.AddScoped<IThanhToanService, ThanhToanService>();
+builder.Services.AddScoped<IDiemDanhService, DiemDanhService>();
 
 var app = builder.Build();
 
@@ -39,6 +56,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAngular");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
