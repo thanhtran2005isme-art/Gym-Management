54b00844d383c2490671b58252829c2283b7c90c
thanhtran2005isme-art <thanhtran2005isme-art@users.noreply.github.com>
2025-12-20 09:30:00 +0700

    update Program đăng ký services

--- GymManagement.API.Admin/Program.cs
+++ GymManagement.API.Admin/Program.cs
@@ -1,8 +1,21 @@
+using GymManagement.DbHelper;
+using GymManagement.API.Admin.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DbHelper
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddSingleton<IDbHelper>(new SqlServerHelper(connectionString!));
+
+// Services
+builder.Services.AddScoped<IChucVuService, ChucVuService>();
+builder.Services.AddScoped<IThanhVienService, ThanhVienService>();
+builder.Services.AddScoped<IGoiTapService, GoiTapService>();
+builder.Services.AddScoped<INhanVienService, NhanVienService>();
 
 var app = builder.Build();
 
