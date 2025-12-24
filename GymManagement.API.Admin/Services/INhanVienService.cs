2c0b448b10739915c35187787a029ccd618a5e51
thanhtran2005isme-art <thanhtran2005isme-art@users.noreply.github.com>
2025-12-24 10:00:00 +0700

    Thêm INhanVienService (Admin)

---
+++ GymManagement.API.Admin/Services/INhanVienService.cs
@@ -0,0 +1,13 @@
+using GymManagement.Shared.DTOs;
+
+namespace GymManagement.API.Admin.Services
+{
+    public interface INhanVienService
+    {
+        Task<IEnumerable<NhanVienDto>> GetAllAsync();
+        Task<NhanVienDto?> GetByIdAsync(int id);
+        Task<NhanVienDto> CreateAsync(NhanVienDto dto);
+        Task<NhanVienDto?> UpdateAsync(int id, NhanVienDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
