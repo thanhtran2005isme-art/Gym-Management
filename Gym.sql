c513fb93f2b55df35209159e866dc512227d48ed
thanhtran2005isme-art <thanhtran2005isme-art@users.noreply.github.com>
2025-12-27 17:00:00 +0700

    cập nhật Program.cs và thêm bảng DangKyGoiTap vào Gym.sql

--- Gym.sql
+++ Gym.sql
@@ -67,3 +67,24 @@ INSERT INTO GoiTap (TenGoiTap, SoThang, Gia, MoTa) VALUES
 (N'Gói 6 tháng', 6, 2000000, N'Gói tập 6 tháng ưu đãi'),
 (N'Gói 12 tháng', 12, 3500000, N'Gói tập 1 năm VIP');
 GO
+
+-- Bảng DangKyGoiTap (Đăng ký gói tập / Membership)
+CREATE TABLE DangKyGoiTap (
+    MaDangKy INT IDENTITY(1,1) PRIMARY KEY,
+    MaThanhVien INT NOT NULL,
+    MaGoiTap INT NOT NULL,
+    NgayDangKy DATE NOT NULL DEFAULT CAST(GETDATE() AS DATE),
+    NgayBatDau DATE NOT NULL,
+    NgayKetThuc DATE NOT NULL,
+    SoLanTapConLai INT NULL,
+    TongTien DECIMAL(18,2) NOT NULL,
+    GhiChu NVARCHAR(255) NULL,
+    TrangThai TINYINT NOT NULL DEFAULT 1,
+    CONSTRAINT FK_DangKy_ThanhVien FOREIGN KEY (MaThanhVien) REFERENCES ThanhVien(MaThanhVien),
+    CONSTRAINT FK_DangKy_GoiTap FOREIGN KEY (MaGoiTap) REFERENCES GoiTap(MaGoiTap)
+);
+GO
+
+CREATE INDEX IX_DangKyGoiTap_ThanhVien ON DangKyGoiTap(MaThanhVien);
+CREATE INDEX IX_DangKyGoiTap_NgayKetThuc ON DangKyGoiTap(NgayKetThuc);
+GO
--- GymManagement.API.Admin/Program.cs
+++ GymManagement.API.Admin/Program.cs
@@ -1,5 +1,6 @@
 using GymManagement.DbHelper;
 using GymManagement.API.Admin.Services;
+using GymManagement.API.Admin.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,11 +15,19 @@ builder.Services.AddSwaggerGen(c =>
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddSingleton<IDbHelper>(new SqlServerHelper(connectionString!));
 
+// Data (Repositories)
+builder.Services.AddScoped<IChucVuRepository, ChucVuRepository>();
+builder.Services.AddScoped<IThanhVienRepository, ThanhVienRepository>();
+builder.Services.AddScoped<IGoiTapRepository, GoiTapRepository>();
+builder.Services.AddScoped<INhanVienRepository, NhanVienRepository>();
+builder.Services.AddScoped<IDangKyGoiTapRepository, DangKyGoiTapRepository>();
+
 // Services
 builder.Services.AddScoped<IChucVuService, ChucVuService>();
 builder.Services.AddScoped<IThanhVienService, ThanhVienService>();
 builder.Services.AddScoped<IGoiTapService, GoiTapService>();
 builder.Services.AddScoped<INhanVienService, NhanVienService>();
+builder.Services.AddScoped<IDangKyGoiTapService, DangKyGoiTapService>();
 
 var app = builder.Build();
 
