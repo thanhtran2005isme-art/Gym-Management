3c51291afa73ea2bff1ed73a699e8ece3e704071
thanhtran2005isme-art <thanhtran2005isme-art@users.noreply.github.com>
2025-12-27 14:00:00 +0700

    Thêm Models cho API.Admin

---
+++ GymManagement.API.Admin/Models/ChucVu.cs
@@ -0,0 +1,9 @@
+namespace GymManagement.API.Admin.Models
+{
+    public class ChucVu
+    {
+        public int MaChucVu { get; set; }
+        public string TenChucVu { get; set; } = null!;
+        public string? MoTa { get; set; }
+    }
+}
