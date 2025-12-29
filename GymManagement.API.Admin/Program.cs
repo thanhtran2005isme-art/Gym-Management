04e34e0bbd48816342f09a47953d3b8e883303b1
thanhtran2005isme-art <thanhtran2005isme-art@users.noreply.github.com>
2025-12-29 10:00:00 +0700

    Cập nhật Solution và csproj

--- GymManagement.API.Admin/Program.cs
+++ GymManagement.API.Admin/Program.cs
@@ -1,16 +1,17 @@
+using GymManagement.DbHelper;
 using GymManagement.API.Admin.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Connection String cho ADO.NET
-var connectionString = builder.Configuration.GetConnectionString("GymDB");
-builder.Services.AddSingleton(connectionString!);
+// DbHelper
+var connectionString = builder.Configuration.GetConnectionString("GymDB")!;
+builder.Services.AddSingleton<IDbHelper>(new SqlServerHelper(connectionString));
 
 // Services
-builder.Services.AddScoped<IChucVuService, ChucVuService>();
+builder.Services.AddScoped<IThanhVienService, ThanhVienService>();
 builder.Services.AddScoped<INhanVienService, NhanVienService>();
 builder.Services.AddScoped<IGoiTapService, GoiTapService>();
-builder.Services.AddScoped<IThanhVienService, ThanhVienService>();
+builder.Services.AddScoped<IChucVuService, ChucVuService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
